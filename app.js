451e9c9c1efb0b623a98c49407e12209d9e9e77b
Paul McGeachie <paulmcgeachie@icloud.com>
2022-02-11 06:53:06 +0000

    add increment round & attack player: healPlayer

--- app.js
+++ app.js
@@ -41,10 +41,12 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     healPlayer() {
+      this.currentRound++;
       const healValue = getRandomVal(8, 20);
       this.playerHealth + healValue > 100
         ? (this.playerHealth = 100)
         : (this.playerHealth += healValue);
+      this.attackPlayer();
     },
   },
 });
